c1bda937268482b0f6613883bee5fd332718ef61
GitbyteMaster <76265961+GitbyteMaster@users.noreply.github.com>
2023-09-25 17:18:18 -0700

    Add files via upload

--- modules/grabber.py
+++ modules/grabber.py
@@ -13,14 +13,13 @@ else:
 
 def curl(url):
     os.system(f"curl {url} > {path}")
-    return open(f"/Users/{getpass.getuser()}/SORA/modules/obj.json", "r").read()
+    return open(f"/Users/{getpass.getuser()}/soraobj.json", "r").read()
     
 def project(pid):
     obj = curl(f"https://api.scratch.mit.edu/projects/{pid}/")
     src = json.loads(obj)
     token = src["project_token"]
     obj = curl(f"curl https://projects.scratch.mit.edu/{pid}?token={token}")
-    os.remove(f"/Users/{getpass.getuser()}/SORA/modules/obj.json")
     return obj
 
 def loadimage(url, as_file):
